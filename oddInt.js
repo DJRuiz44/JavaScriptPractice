a6a408930c7b076ba4c36dee604a98db1b9baf7a
Daniel Ruiz <djruiz@umail.iu.edu>
2025-12-02 18:53:57 -0500

    6 kyu exercise....  We are well on the way my friend

---
+++ oddInt.js
@@ -0,0 +1,19 @@
+function findOdd(A) {
+  //happy coding!
+  let frequency = {};
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] in frequency) {
+      frequency[A[i]] += 1;
+    } else {
+      frequency[A[i]] = 1;
+    }
+  }
+  //console.log(frequency);
+  for (let key in frequency) {
+    if (frequency[key] % 2) {
+      return parseInt(key);
+    }
+  }
+}
+
+console.log(findOdd([0, 1, 2, 3, 0, 1, 3]));
