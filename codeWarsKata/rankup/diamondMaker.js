0a74f30c02c581a851b357128aa1aa157e6d036a
DJRuiz44 <djruiz44@gmail.com>
2025-12-03 10:56:40 -0500

    CodeWars rank up kata - diamonds are formed under pressure.  You a diamond mud

---
+++ codeWarsKata/rankup/diamondMaker.js
@@ -0,0 +1,16 @@
+function diamond(n){
+  if(n % 2 == 0 || n < 0){
+    return null;
+  }
+  let diamond = "";
+  for(let i = 1; i <= n; i+=2){
+    diamond += " ".repeat((n-i)/2) + "*".repeat(i) + "\n";
+  }
+  for(let i=n-2; i>0; i-=2){
+    diamond += " ".repeat((n-i)/2) + "*".repeat(i) + "\n";
+  }
+  return diamond;
+}
+console.log(diamond(11));
+console.log(diamond(19));
+console.log(diamond(21));
