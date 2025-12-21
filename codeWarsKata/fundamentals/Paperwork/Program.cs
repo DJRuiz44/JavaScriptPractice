7e4a25bf200a9f6cea0a4ebb12fbdfdfaf7103d5
DJRuiz44 <djruiz44@gmail.com>
2025-12-21 11:21:48 -0500

    started doing CW fundamentals elsewhere

---
+++ codeWarsKata/fundamentals/Paperwork/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int Paperwork(int n, int m)
+{
+    if (n < 0 || m < 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n * m;
+    }
+}
+
+Console.WriteLine(Paperwork(5, 5));
