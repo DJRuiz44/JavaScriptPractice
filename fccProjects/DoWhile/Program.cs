72c74d8a315dfaceac55665c1cb66d097ca47836
DJRuiz44 <djruiz44@gmail.com>
2025-12-23 19:58:18 -0500

    Dice simulator for fcc dowhile section

---
+++ fccProjects/DoWhile/Program.cs
@@ -0,0 +1,8 @@
+﻿Random random = new Random();
+int roll = random.Next(0, 7);
+do
+{
+    roll = random.Next(0, 7);
+    Console.WriteLine($"You rolled a {roll}! try again");
+} while(roll != 6);
+Console.WriteLine("You crit! GG");
